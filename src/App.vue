03d134515be36974c9bb844bbfbff6e7fcc58892
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2022-11-04 17:33:54 +0900

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,18 @@
 <template>
-    <nav>
-    <router-link to="/">1st</router-link> |
-    <router-link to="/second">SECOND</router-link>
-  </nav>
-  <router-view />
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+
 export default {
-  name: 'App'
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
