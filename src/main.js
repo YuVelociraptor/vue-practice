50a2c0711837207d08913ba96c46be0d3f668e32
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2023-01-06 08:50:19 +0900

    uninstall quasar

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
-import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
 
-createApp(App).use(Quasar, quasarUserOptions).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
